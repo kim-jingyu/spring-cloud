53c784ab07e9fe1729063a714f26fcbf53a1feaf
JingyuKim <swc05161@gmail.com>
2023-07-18 17:47:21 +0900

    chore: Circuit Breaker Framework - Resilience4j

---
+++ src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,24 @@
+package com.microservices.currencyexchangeservice;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class CircuitBreakerController {
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        log.info("Sample api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception exception) {
+        return "fallback-response";
+    }
+}
